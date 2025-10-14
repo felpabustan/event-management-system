91f4f30b24e79a316a1b6950bc5f451008683386
t0nchie <felpabustan93@gmail.com>
2025-10-14 16:35:43 +0800

    remove qr code on email notification

--- resources/views/emails/attendee-confirmation.blade.php
+++ resources/views/emails/attendee-confirmation.blade.php
@@ -22,16 +22,9 @@ Thank you for registering for **{{ $event->title }}**!
 **Phone:** {{ $registration->phone }}  
 @endif
 
-## Check-in QR Code
-
-Please bring this QR code with you to the event for quick check-in:
-
-<div style="text-align: center; margin: 20px 0;">
-    {!! $qrCode !!}
-</div>
 Check-in Token: {{ $token }}
 
-**Important:** This QR code is unique to your registration. Please do not share it with others.
+**Important:** This Check-in Token code is unique to your registration. Please do not share it with others.
 
 We look forward to seeing you at the event!
 
