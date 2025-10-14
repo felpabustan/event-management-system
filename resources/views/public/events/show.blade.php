0ccbc5f53590147f54344bb762b31257ea40db9c
t0nchie <felpabustan93@gmail.com>
2025-10-14 09:35:13 +0800

    hide sections on show event page

--- resources/views/public/events/show.blade.php
+++ resources/views/public/events/show.blade.php
@@ -60,7 +60,7 @@
                                         </div>
                                     </div>
                                     
-                                    <div class="flex items-center">
+                                    {{-- <div class="flex items-center">
                                         <svg class="flex-shrink-0 mr-3 h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                         </svg>
@@ -68,7 +68,7 @@
                                             <p class="text-sm font-medium text-gray-900">Time</p>
                                             <p class="text-sm text-gray-500">{{ \Carbon\Carbon::createFromFormat('H:i:s', $event->time)->format('g:i A') }}</p>
                                         </div>
-                                    </div>
+                                    </div> --}}
                                     
                                     <div class="flex items-center">
                                         <svg class="flex-shrink-0 mr-3 h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -81,7 +81,7 @@
                                         </div>
                                     </div>
                                     
-                                    @if($event->category)
+                                    {{-- @if($event->category)
                                     <div class="flex items-center">
                                         <svg class="flex-shrink-0 mr-3 h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"></path>
@@ -97,7 +97,7 @@
                                             </p>
                                         </div>
                                     </div>
-                                    @endif
+                                    @endif --}}
                                     
                                     <div class="flex items-center">
                                         <svg class="flex-shrink-0 mr-3 h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
